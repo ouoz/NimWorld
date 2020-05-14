c89c5b9214f496f53f02d30a17bf5608fc530e88
ouoz <ouoz1V@gmail.com>
2020-05-14 21:24:41 +0900

    snippet dayo

--- 1.nim
+++
@@ -1,8 +0,0 @@
-import sequtils, strutils, algorithm, math, future, sets, tables, hashes, intsets
-let read = iterator : string {.closure.} = (while true : (for s in stdin.readLine.split : yield s))
-
-
-
-
-
-
