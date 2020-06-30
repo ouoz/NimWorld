10000d574b94fc89f6095abc4275682babf03492
ouoz <ouoz1v@gmail.com>
2020-06-30 15:16:04 +0900

    test

--- yey.nim
+++ yey.nim
@@ -1,58 +1,50 @@
-import sequtils, strutils, algorithm, math, future, sets, tables, hashes, intsets, queues
+import sequtils, strutils, algorithm, math, future, sets, tables, hashes, intsets
 let read = iterator : string {.closure.} = (while true : (for s in stdin.readLine.split : yield s))
 
 
-var
-  s = read()
-  used = initQueue[tuple[a,b: int]]()
-  temoto = 5
-  combo = -1
-  ans = 0
-  charge = initQueue[int]()
-  chargeflag = false
-  pre = int.low
-
-s = s & "----------"
-
-for i in 0 ..< s.len + 10:
+const Mod = 1000000007
+const LowZ = int.low div Mod*Mod
+type mint = distinct int
+converter toMint*(a: int): mint = mint((a-int(a<0)*LowZ)mod Mod)
+proc `+`(a,b: mint): mint = a.int + b.int
+proc `+=`(a: var mint; b: mint) = a = a + b
+proc `*`(a,b: mint): mint = a.int * b.int
+proc pow(a: mint, x: int): mint =
+  assert(x >= 0)
   var
-    gabun = used.len
-  for j in 0 ..< gabun:
-    var v = used.dequeue
-    if v.a <= i * 2:
-      temoto += v.b
-    else:
-      used.enqueue((v.a, v.b)) 
+    res = 1.toMint
+    base = a
+    exp = x
+  while exp > 0:
+    if (exp and 1) == 1:
+      res = res * base
+    base = base * base
+    exp = exp div 2
+  return res
+proc inv(a: mint): mint = pow(a, Mod - 2)
+proc `div`(a, b: mint): mint = a * inv(b)
+proc `$`(a: mint): string {.borrow.}
 
-  if charge.len != 0:
-    var
-      cur = charge.dequeue
-    if cur <= i * 2:
-      ans += 50 + combo div 10 * 5
-      combo += 1
-      chargeflag = false
-    else:
-      charge.enqueue cur
 
-  if s[i] == 'N' and not chargeflag and pre != i - 1:
-    combo += 1
-    if temoto >= 1:
-      temoto -= 1
-      used.enqueue((i * 2 + 13, 1))
-      ans += 10 + combo div 10
-      pre = i
-  elif s[i] == 'N' and not chargeflag and pre == i - 1:
-    if temoto >= 1:
-      temoto -= 1
-      used.enqueue((i * 2 + 13, 1))
-      ans += 10 + combo div 10
-      combo += 1
-  elif s[i] == 'C' and not chargeflag:
-    if temoto >= 3:
-      gabun = used.len
-      temoto -= 3
-      used.enqueue((i * 2 + 18, 3))
-      charge.enqueue(i + 5)
-      chargeflag = true
-echo ans
 
+template yieldCombination(n: int = 20) =
+  var
+    fac {.inject.} = newSeq[mint](n)
+    inv {.inject.} = newSeq[mint](n)
+  fac[0] = 1.toMint
+  for i in 1..<n:
+    fac[i] = i.toMint * fac[i - 1]
+  inv[n - 1] = 1.toMint div fac[n - 1]
+  for i in countdown(n - 2, 0):
+    inv[i] = inv[i + 1] * (i + 1).toMint
+  let binom {.inject.} =
+    proc(a, b: int): mint =
+      if a < 0 or b < 0: return 0.toMint
+      if a < b: return 0.toMint
+      return fac[a] * inv[b] * inv[a - b]
+
+
+
+var
+  n, m = read().parseint
+  
